ff2630bf98f3f3183cf4d7078f29ff345b6ef84e
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-10-12 13:40:20 -0700

    moved login form to seaprate file instead of app.js

---
+++ client/src/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AmplifyAuthenticator, AmplifySignIn } from '@aws-amplify/ui-react';
+import './login.css';
+import AuthStateApp from './authstate';
+
+const Login = () => {
+  return (
+    <AmplifyAuthenticator usernameAlias="email">
+      <AmplifySignIn slot="sign-in" usernameAlias="email" 
+      />
+      <AuthStateApp />
+    </AmplifyAuthenticator>
+  );
+};
+
+export default Login;
