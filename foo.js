9c632bc07dd112efb64032e782bf79d63c734dee
Rob Kellen <rob.h.kellen@gmail.com>
2020-10-13 14:46:34 -0700

    deleted user model and api routes as AWS amplify will handle all of our users

--- foo.js
+++ foo.js
@@ -3,13 +3,6 @@ const db = require("./models");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/campK9");
 
-db.User.create({
-  firstName: "Rob",
-  lastName: "Kellen",
-  email: "rob@email.com",
-  password: "abbazabba",
-}).then((dbUser) => console.log(dbUser));
-
 db.Dog.create({
   name: "DOG",
   breed: "Husky",
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
 const dogRoutes = require("./dogs");
-const userRoutes = require("./user");
+
 
 //Dog routes
 router.use("/dogs", dogRoutes);
 
-//User routes
-// router.use("/user", userRoutes);
 
 module.exports = router;
