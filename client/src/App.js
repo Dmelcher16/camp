efc1db92c1549de9651dc850e072e174cac6d4f7
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-10-13 14:16:14 -0700

    created login page and linked pages in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
 import React from "react";
-import "./App.css";
-import AppNav from "./components/AppNav/AppNav.js";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-import Footer from "./components/Footer/Footer";
-import Login from "./components/Login/Login";
+// import "./App.css";
+// import AppNav from "./components/AppNav/AppNav.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AboutPage from "./components/AboutPage/AboutPage";
+import HomePage from "./components/HomePage/HomePage";
+// import Footer from "./components/Footer/Footer";
+// import Login from "./components/Login/Login";
+// import HomePage from "./components/HomePage/HomePage";
+import loginPage from "./components/loginPage/loginPage";
 // import AboutPage from "./components/AboutPage/AboutPage.js";
 // import HomePage from "./components/HomePage/HomePage.js";
 
 const App = () => {
   return (
     <Router>
-      <AppNav />
+      <div>
+       <Route exact path="/" component={loginPage} />
+       <Route exact path="/about" component={AboutPage} />
+       <Route exact path="/home" component={HomePage} />
+      </div>
+
+      {/* <AppNav />
       <div className="main">
       <Login />
       </div>
-      {/* <Route exact path="/" component={HomePage} />
-      <Route exact path="/about" component={AboutPage} />
-      <Link to="/">Go home</Link>
-      <Link to="/about">go to about</Link> */}
-      <Footer />
+      <Footer /> */}
+      
     </Router>
   );
 };
