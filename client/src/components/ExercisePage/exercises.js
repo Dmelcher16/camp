70d63fcbb77d4f1e94b5d2595969f3e384b3886b
Rob Kellen <rob.h.kellen@gmail.com>
2020-12-02 11:56:09 -0700

    added white background to exercise form.  Changed the way inputs would render based on the exercise type that was selected

--- client/src/components/ExercisePage/exercises.js
+++ client/src/components/ExercisePage/exercises.js
@@ -6,7 +6,7 @@ import { Label, Input, Select, FormBtn } from "./createExerciseForm";
 import API from "../../utils/exerciseAPI";
 
 export default function ExercisePage() {
-  const [dogs, setExercises] = useState([]); 
+  const [exercises, setExercises] = useState([]); // eslint-disable-line
   const [createExercise, setCreateExercise] = useState({
     Exercises: "",
     leashDuration: "",
@@ -17,22 +17,25 @@ export default function ExercisePage() {
     commandsCompleted: "",
     chewing: "",
     numPottyAccidents: "",
-    numPottySuccesses: ""
+    numPottySuccesses: "",
   });
-  
+
+  const exerciseTypeSelect = document.querySelector("#type");
+  const leashTrainingForm = document.querySelector(".leash-training");
+  const sitStayForm = document.querySelector(".sit-stay-form");
+  const commandsForm = document.querySelector(".commands-form");
+  const chewingForm = document.querySelector(".chewing");
+  const pottyForm = document.querySelector(".potty-form");
 
   useEffect(() => {
     loadDogExercises();
-   
   }, []);
 
   function loadDogExercises() {
     API.getExercises()
       .then((res) => setExercises(res.data))
-      .catch((err) => console.log(err))
+      .catch((err) => console.log(err));
   }
-
-  
 
   function handleInputChange(event) {
     const { name, value } = event.target;
@@ -40,14 +43,66 @@ export default function ExercisePage() {
   }
   console.log(createExercise);
 
-  // function handleBreedChange(event) {
-  //   const {name, value} = event.target;
+  function handleExerciseChange(event) {
+    const exerciseType = event.target.value;
 
-  // }
+    if (exerciseType === "Sit/Stay") {
+      sitStayForm.classList.remove("d-none");
+      leashTrainingForm.classList.add("d-none");
+      commandsForm.classList.add("d-none");
+      chewingForm.classList.add("d-none");
+      pottyForm.classList.add("d-none");
+    } else if (exerciseType === "Leash Training") {
+      leashTrainingForm.classList.remove("d-none");
+      sitStayForm.classList.add("d-none");
+      commandsForm.classList.add("d-none");
+      chewingForm.classList.add("d-none");
+      pottyForm.classList.add("d-none");
+    } else if (exerciseType === "Commands") {
+      commandsForm.classList.remove("d-none");
+      sitStayForm.classList.add("d-none");
+      leashTrainingForm.classList.add("d-none");
+      chewingForm.classList.add("d-none");
+      pottyForm.classList.add("d-none");
+    } else if (exerciseType === "Chewing") {
+      chewingForm.classList.remove("d-none");
+      sitStayForm.classList.add("d-none");
+      commandsForm.classList.add("d-none");
+      pottyForm.classList.add("d-none");
+      leashTrainingForm.classList.add("d-none");
+    } else if (exerciseType === "Potty Training") {
+      pottyForm.classList.remove("d-none");
+      sitStayForm.classList.add("d-none");
+      leashTrainingForm.classList.add("d-none");
+      commandsForm.classList.add("d-none");
+      chewingForm.classList.add("d-none");
+    } else {
+      pottyForm.classList.add("d-none");
+      sitStayForm.classList.add("d-none");
+      leashTrainingForm.classList.add("d-none");
+      commandsForm.classList.add("d-none");
+      chewingForm.classList.add("d-none");
+    }
+  }
 
-  function handleFormSubmit(event){
+  if (exerciseTypeSelect) {
+    exerciseTypeSelect.addEventListener("change", handleExerciseChange);
+  }
+
+  function handleFormSubmit(event) {
     event.preventDefault();
-    if (createExercise.Exercises && createExercise.leashDuration && createExercise.leashPullDuration && createExercise.sitStayAttempts && createExercise.sitStaySuccess && createExercise.commandsAttempted && createExercise.commandsCompleted && createExercise.chewing && createExercise.numPottyAccidents && createExercise.numPottySuccesses) {
+    if (
+      createExercise.Exercises ||
+      createExercise.leashDuration ||
+      createExercise.leashPullDuration ||
+      createExercise.sitStayAttempts ||
+      createExercise.sitStaySuccess ||
+      createExercise.commandsAttempted ||
+      createExercise.commandsCompleted ||
+      createExercise.chewing ||
+      createExercise.numPottyAccidents ||
+      createExercise.numPottySuccesses
+    ) {
       API.addExercise({
         Exercises: createExercise.Exercises,
         leashDuration: createExercise.leashDuration,
@@ -58,11 +113,11 @@ export default function ExercisePage() {
         commandsCompleted: createExercise.commandsCompleted,
         chewing: createExercise.chewing,
         numPottyAccidents: createExercise.numPottyAccidents,
-        numPottySuccesses: createExercise.numPottySuccesses
+        numPottySuccesses: createExercise.numPottySuccesses,
       })
-      .then(alert(`${createExercise.Exercise} has been added to your list!`))
-      .then((res) => loadDogExercises())
-      .catch((err) => console.log(err))
+        .then(alert(`${createExercise.Exercise} has been added to your list!`))
+        .then((res) => loadDogExercises())
+        .catch((err) => console.log(err));
     }
   }
 
@@ -70,101 +125,113 @@ export default function ExercisePage() {
     <div className="FormPageImg">
       <AppNav />
       <h1>Add your exercises!</h1>
-      <Row>
+      <Row className="justify-content-center">
         <Col md="6">
-          <Container fluid>
-
-          <form>
-            <Label>Exercise:</Label>
-            <Select
-              onChange={handleInputChange}
-              value={createExercise.Exercises}
-              name="Exercises"
-              placeholder="Exercise Name (required)"
-            />
-            <Label>Walk Time:</Label>
-            <Input
-              onChange={handleInputChange}
-              value={createExercise.leashDuration}
-              name="leashDuration"
-              placeholder="Length of walk in minutes (required)"
-            />
-            <Label>Leash Pulls:</Label>
-            <Input
-              onChange={handleInputChange}
-              value={createExercise.leashPullDuration}
-              name="leashPullDuration"
-              placeholder="Length of time spent pulling in minutes (required)"
-            />
-            <Label>Sit/Stay attempts:</Label>
-            <Input
-              onChange={handleInputChange}
-              value={createExercise.sitStayAttempts}
-              name="sitStayAttempts"
-              placeholder="Number of attempted sit/stay commands (Required)"
-            />
-            <Label>Sit/stay Successes:</Label>
-            <Input
-              onChange={handleInputChange}
-              value={createExercise.sitStaySuccess}
-              name="sitStaySuccess"
-              placeholder="Number of times successfully sit/stayed (Required)"
-            />
-            <Label>Commands Attempted:</Label>
-            <Input
-              onChange={handleInputChange}
-              value={createExercise.commandsAttempted}
-              name="commandsAttempted"
-              placeholder="Number of commands attempted (Required)"
-            />
-            <Label>Commands Completed:</Label>
-            <Input
-              onChange={handleInputChange}
-              value={createExercise.commandsCompleted}
-              name="commandsCompleted"
-              placeholder="Number of commands completed (Required)"
-            />
-            <Label>Chewing:</Label>
-            <Input
-              onChange={handleInputChange}
-              value={createExercise.chewing}
-              name="chewing"
-              placeholder="Number of items chewed up today (Required)"
-            />
-            <Label>Potty Accidents:</Label>
-            <Input
-              onChange={handleInputChange}
-              value={createExercise.numPottyAccidents}
-              name="numPottyAccidents"
-              placeholder="Number of accidents today (Required)"
-            />
-            <Label>Successful Potty Breaks:</Label>
-            <Input
-              onChange={handleInputChange}
-              value={createExercise.numPottySuccesses}
-              name="numPottySuccesses"
-              placeholder="Number of successful potty breaks (Required)"
-            />
-            <FormBtn
-              disabled={
-                !(
-                    createExercise.Exercises &&
-                    createExercise.leashDuration &&
-                    createExercise.leashPullDuration &&
-                    createExercise.sitStayAttempts &&
-                    createExercise.sitStaySuccess &&
-                    createExercise.commandsAttempted &&
-                    createExercise.commandsCompleted &&
-                    createExercise.chewing &&
-                    createExercise.numPottyAccidents &&
+          <Container fluid className="form-container">
+            <form>
+              <div>
+                <Label>Exercise:</Label>
+                <Select
+                  onChange={handleInputChange}
+                  value={createExercise.Exercises}
+                  name="Exercises"
+                  placeholder="Exercise Name (required)"
+                  id="type"
+                />
+              </div>
+              <div className="d-none leash-training">
+                <Label>Walk Time:</Label>
+                <Input
+                  onChange={handleInputChange}
+                  value={createExercise.leashDuration}
+                  name="leashDuration"
+                  placeholder="Length of walk in minutes (required)"
+                />
+                <Label>Leash Pulls:</Label>
+                <Input
+                  onChange={handleInputChange}
+                  value={createExercise.leashPullDuration}
+                  name="leashPullDuration"
+                  placeholder="Length of time spent pulling in minutes (required)"
+                />
+              </div>
+              <div className="d-none sit-stay-form">
+                <Label>Sit/Stay attempts:</Label>
+                <Input
+                  onChange={handleInputChange}
+                  value={createExercise.sitStayAttempts}
+                  name="sitStayAttempts"
+                  placeholder="Number of attempted sit/stay commands (Required)"
+                />
+                <Label>Sit/stay Successes:</Label>
+                <Input
+                  onChange={handleInputChange}
+                  value={createExercise.sitStaySuccess}
+                  name="sitStaySuccess"
+                  placeholder="Number of times successfully sit/stayed (Required)"
+                />
+              </div>
+              <div className="d-none commands-form">
+                <Label>Commands Attempted:</Label>
+                <Input
+                  onChange={handleInputChange}
+                  value={createExercise.commandsAttempted}
+                  name="commandsAttempted"
+                  placeholder="Number of commands attempted (Required)"
+                />
+                <Label>Commands Successfully Completed:</Label>
+                <Input
+                  onChange={handleInputChange}
+                  value={createExercise.commandsCompleted}
+                  name="commandsCompleted"
+                  placeholder="Number of commands completed (Required)"
+                />
+              </div>
+              <div className="d-none chewing">
+                <Label>Chewing:</Label>
+                <Input
+                  onChange={handleInputChange}
+                  value={createExercise.chewing}
+                  name="chewing"
+                  placeholder="Number of items chewed up today (Required)"
+                />
+              </div>
+              <div className="d-none potty-form">
+                <Label>Potty Accidents:</Label>
+                <Input
+                  onChange={handleInputChange}
+                  value={createExercise.numPottyAccidents}
+                  name="numPottyAccidents"
+                  placeholder="Number of accidents today (Required)"
+                />
+                <Label>Successful Potty Breaks:</Label>
+                <Input
+                  onChange={handleInputChange}
+                  value={createExercise.numPottySuccesses}
+                  name="numPottySuccesses"
+                  placeholder="Number of successful potty breaks (Required)"
+                />
+              </div>
+              <FormBtn
+                disabled={
+                  !(
+                    createExercise.Exercises ??
+                    createExercise.leashDuration ??
+                    createExercise.leashPullDuration ??
+                    createExercise.sitStayAttempts ??
+                    createExercise.sitStaySuccess ??
+                    createExercise.commandsAttempted ??
+                    createExercise.commandsCompleted ??
+                    createExercise.chewing ??
+                    createExercise.numPottyAccidents ??
                     createExercise.numPottySuccesses
-                )
-              }
-              onClick={handleFormSubmit}
-            >
-              Complete
-            </FormBtn>
-          </form>
+                  )
+                }
+                onClick={handleFormSubmit}
+              >
+                Complete
+              </FormBtn>
+            </form>
           </Container>
         </Col>
       </Row>
