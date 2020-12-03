f35f836a7b38b396af2c4f5688e5550d1a521740
Rob Kellen <rob.h.kellen@gmail.com>
2020-12-03 14:15:59 -0700

    created dog detail page to render info on dog when clicked.  tweaked height of links in navbar.

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,10 @@ import AboutPage from "./components/AboutPage/AboutPage.js";
 import HomePage from "./components/HomePage/HomePage.js";
 import loginPage from "./components/loginPage/loginPage";
 import FormPage from "./components/FormPage/FormPage";
-import ExercisePage from './components/ExercisePage/exercises'
+import ExercisePage from "./components/ExercisePage/exercises";
 import chartPage from "./components/ChartPage/chartModel";
-import dataPage from './components/ChartPage/dataPage'
+import dataPage from "./components/ChartPage/dataPage";
+import DogPage from "./components/DogPage/DogPage";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
       <Route exact path="/chart" component={chartPage} />
       <Route exact path="/data" component={dataPage} />
       <Route exact path="/exercise" component={ExercisePage} />
+      <Route exact path="/dog/:id" component={DogPage} />
       <Footer />
     </Router>
   );
--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -6,9 +6,6 @@ import { IoIosAddCircleOutline } from "react-icons/io";
 import AppNav from "../AppNav/AppNav.js";
 import API from "../../utils/API";
 
-
-
-
 function Homepage() {
   //set initial state
   const [dogs, setDogs] = useState([]);
@@ -29,45 +26,55 @@ function Homepage() {
       <AppNav />
       <Container>
         {/* <div className="card-container"> */}
-          <h1>
-            My Kennel{" "}
-            <Link to="/form">
-              <IoIosAddCircleOutline />{" "}
-            </Link>
-          </h1>
-          <Row className="row">
-            <Col>
-              <CardGroup>
-                <Row id="mapRow">
-                  {dogs.map((dog) => (
-                    <div key={dog._id} className="card-deck">
-                      <Col key={dog._id} mb="3">
-                        <Card
-                          key={dog._id}
-                          style={{ width: "18rem", text: "center" }}
-                        >
-                          <Card.Img
+        <h1>
+          My Kennel{" "}
+          <Link to="/form">
+            <IoIosAddCircleOutline />{" "}
+          </Link>
+        </h1>
+        <Row className="row justify-content-center">
+          <Col>
+            <CardGroup>
+              <Row id="mapRow">
+                {dogs.map((dog) => (
+                  <div key={dog._id} className="card-deck">
+                    <Col key={dog._id} mb="3">
+                      <Card
+                        key={dog._id}
+                        style={{ width: "18rem", text: "center" }}
+                      >
+                        <Link to={"/dog/" + dog._id}>
+                          <img
                             key={dog.image}
+                            alt={dog.name}
                             variant="top"
                             src={dog.image}
                             className="card-img-top"
                           />
-                          <Card.Body key={dog._id}>
-                            <Card.Title className="dogName" key={dog.name}>
-                              {dog.name}
-                            </Card.Title>
-                            <Link className="cardLink" to="/exercise">Add daily exercises</Link>
+                        </Link>
+                        <Card.Body key={dog._id}>
+                          <Card.Title
+                            className="dogName text-center"
+                            key={dog.name}
+                          >
+                            {dog.name}
+                          </Card.Title>
+                          <Link className="cardLink" to="/exercise">
+                            Add daily exercises
+                          </Link>
                           <br></br>
-                          <Link className="cardLink" to="/chart">View progress</Link>
-                          </Card.Body>
-                        </Card>
-                      </Col>
-                    </div>
-                  ))}
-                </Row>
-              </CardGroup>
-            </Col>
-          </Row>
+                          <Link className="cardLink" to="/chart">
+                            View progress
+                          </Link>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  </div>
+                ))}
+              </Row>
+            </CardGroup>
+          </Col>
+        </Row>
         {/* </div> */}
       </Container>
     </div>
