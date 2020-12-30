18b68e6d2de74f37868b33f33b368c7d88ab00c1
Rob Kellen <rob.h.kellen@gmail.com>
2020-12-29 17:29:47 -0700

    edited logo source in login page

--- client/src/pages/loginPage/loginPage.js
+++ client/src/pages/loginPage/loginPage.js
@@ -2,13 +2,14 @@ import React from "react";
 import Login from "../../components/Login/Login";
 import "./loginPage.css";
 import { Image } from "react-bootstrap";
+import BlueCampK9Logo from "../../images/BlueCampK9Logo.jpeg"
 
 export default function loginPage() {
   return (
     <div>
       <style type="text/css">{`#app-nav {display: none}`}</style>
       <div className="loginPageImg">
-        <Image className="logo" src="./BlueCampK9Logo.jpeg" fluid />
+        <Image className="logo" src={BlueCampK9Logo} fluid />
         <Login />
       </div>
     </div>
