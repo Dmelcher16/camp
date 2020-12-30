fd9f02911f9104e2632e4f98d1c7798d1f12baa9
Rob Kellen <rob.h.kellen@gmail.com>
2020-12-30 15:42:02 -0700

    added index.html to get all route in server.js line 35

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const routes = require("./routes");
 const cors = require("cors");
 const app = express();
 
-
 const PORT = process.env.PORT || 8080;
 
 //requiring morgan to use as dev tool
@@ -29,11 +28,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/campK9", {
   useCreateIndex: true,
 });
 
-// Serve up static assets 
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname + "/client/build"));
+    res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
   });
 }
 
