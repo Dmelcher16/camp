48e0768146ca72042b36562e014ba5340cd18522
Rob Kellen <rob.h.kellen@gmail.com>
2020-12-31 13:53:29 -0700

    trying to resolve manifest.json syntax error

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/campK9", {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
 }
 
