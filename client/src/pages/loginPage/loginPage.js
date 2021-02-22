de1d5cc54b8e9d1ac37252678639cc8d66493201
Rob Kellen <rob.h.kellen@gmail.com>
2021-02-22 16:18:13 -0700

    formatted login component to be within a card with CampK9 logo at the top

--- client/src/pages/loginPage/loginPage.js
+++ client/src/pages/loginPage/loginPage.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 import Login from "../../components/Login/Login";
+import BlueCampK9Logo from "../../images/BlueCampK9Logo.jpeg";
 import "./loginPage.css";
 
 import Footer from "../../components/Footer/Footer";
@@ -11,10 +12,30 @@ function LoginPage() {
       <div className="loginPageImg">
         <Container>
           <Row>
-            <Col md={{ span: 4, offset: 4 }}>
-              <Login />
+            <Col
+              className="login-col text-center"
+              lg={{ span: 6, offset: 6 }}
+              sm={{ span: 12 }}
+            >
+              <Card id="login-card">
+                <div className="card-image-top">
+                  <img
+                    className="logo"
+                    src={BlueCampK9Logo}
+                    alt="Brand Logo"
+                  ></img>
+                </div>
+                <div className="card-body login-card-body">
+                  <Login />
+                </div>
+              </Card>
             </Col>
           </Row>
+          {/* <Row>
+            <Col className="login-col text-center" lg={{ span: 4, offset: 8 }}>
+              <Login />
+            </Col>
+          </Row> */}
         </Container>
       </div>
       <Footer />
