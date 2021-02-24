942806863635784cbb294c25171b40dcde6a74cf
Rob Kellen <rob.h.kellen@gmail.com>
2021-02-24 12:37:13 -0700

    added lorem welcome paragraph (will convert to text about the application) on login page with buttons to learn more section (to come) and to show login form based on state of showLogin

--- client/src/components/WelcomeNav/WelcomeNav.js
+++ client/src/components/WelcomeNav/WelcomeNav.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
-import BlueCampK9Logo from "../../images/BlueCampK9_76.jpeg";
+import Logo from "../../images/BlueCampK9Logo.jpeg";
 import "./WelcomeNav.css";
 
 export default function WelcomeNav() {
   return (
     <Navbar sticky="top" variant="light" expand="sm" id="welcome-nav">
-      <Navbar.Brand className="navbar-brand mb-1 h1">
-        <img src={BlueCampK9Logo} alt="Brand Logo"></img>
+      <Navbar.Brand className="navbar-brand mb-1 h1" id="welcome-brand">
+        <img  src={Logo} alt="Brand Logo"></img>
       </Navbar.Brand>
-      <h5 id="welcome-h5">CAMP K-9</h5>
+      <h4 id="welcome-h5">Showcase Your Training Progress.</h4>
     </Navbar>
   );
 }
--- client/src/pages/loginPage/loginPage.js
+++ client/src/pages/loginPage/loginPage.js
@@ -1,47 +1,87 @@
-import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import React, { useContext, useState } from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import WelcomeNav from "../../components/WelcomeNav/WelcomeNav";
 import Login from "../../components/Login/Login";
-import BlueCampK9Logo from "../../images/BlueCampK9Logo.jpeg";
+import BlueCampK9Logo from "../../images/BlueCampK9_76.jpeg";
 import "./loginPage.css";
-
+import LoginContext from "../../utils/loginContext";
 import Footer from "../../components/Footer/Footer";
 
 function LoginPage() {
+  //set initial state
+  const [showLogin, setShowLogin] = useState(false);
+
+  // useEffect(() => {
+  //   setShowLogin(false);
+  // }, [showLogin]);
+
   return (
-    <div>
-      <div className="loginPageImg">
-        <WelcomeNav />
-        <Container>
-          <Row id="login-row">
-            <Col
-              className="login-col text-center"
-              lg={{ span: 6, offset: 6 }}
-              sm={{ span: 12 }}
-            >
-              <Card id="login-card">
-                <div className="card-image-top">
-                  <img
-                    className="logo"
-                    src={BlueCampK9Logo}
-                    alt="Brand Logo"
-                  ></img>
-                </div>
-                <div className="card-body login-card-body">
-                  <Login />
-                </div>
-              </Card>
-            </Col>
-          </Row>
-          {/* <Row>
+    <LoginContext.Provider value={{ setShowLogin }}>
+      <div>
+        <div className="loginPageImg">
+          <div className="overlay"></div>
+          <div className="content">
+            <WelcomeNav />
+            <Container id="login-container">
+              <Row id="login-row">
+                <Col
+                  className="login-col text-center"
+                  lg={{ span: 6, offset: 6 }}
+                  sm={{ span: 12 }}
+                >
+                  {!showLogin ? (
+                    <div>
+                      <h2>Welcome to Camp K-9!</h2>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Integer posuere erat a ante.Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit. Integer posuere erat a
+                        ante.Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit. Integer posuere erat a ante.Lorem ipsum dolor sit
+                        amet, consectetur adipiscing elit. Integer posuere erat
+                        a ante.Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit. Integer posuere erat a ante.
+                      </p>
+                      <div className="inline">
+                        <Button id="to-kennel" variant="outline-success">
+                          Learn More
+                        </Button>
+                        <Button
+                          id="to-kennel"
+                          variant="outline-success"
+                          onClick={() => setShowLogin(true)}
+                        >
+                          Login/Sign-Up
+                        </Button>
+                      </div>
+                    </div>
+                  ) : (
+                    <Card id="login-card">
+                      <div className="card-image-top">
+                        <img
+                          className="logo"
+                          src={BlueCampK9Logo}
+                          alt="Brand Logo"
+                        ></img>
+                      </div>
+                      <div className="card-body login-card-body">
+                        <Login />
+                      </div>
+                    </Card>
+                  )}
+                </Col>
+              </Row>
+              {/* <Row>
             <Col className="login-col text-center" lg={{ span: 4, offset: 8 }}>
               <Login />
             </Col>
           </Row> */}
-        </Container>
+            </Container>
+          </div>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </LoginContext.Provider>
   );
 }
 
