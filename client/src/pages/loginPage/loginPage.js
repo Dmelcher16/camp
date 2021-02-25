7d4848ad6a29160aa1922894b67f8f2300650b2d
Rob Kellen <rob.h.kellen@gmail.com>
2021-02-25 14:20:05 -0700

    added react-scroll npm package and made Learn More button on landing page functional to scroll to first div in about-us section

--- client/src/pages/loginPage/loginPage.js
+++ client/src/pages/loginPage/loginPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import Login from "../../components/Login/Login";
+import { Link } from "react-scroll";
 import Logo76x76 from "../../images/BlueCampK9_76.jpeg";
 import Logo198x198 from "../../images/BlueCampK9_198.jpeg";
 import PupHighFive from "../../images/tamu_obedience.jpg";
@@ -36,9 +37,17 @@ function LoginPage() {
                         Helping You Showcase Your Pup-Training Progress.
                       </h1>
                       <div className="inline">
-                        <Button id="learn-more-btn" variant="outline-light">
-                          Learn More
-                        </Button>
+                        <Link
+                          to="about-us"
+                          spy={true}
+                          smooth={true}
+                          // offset={10}
+                          duration={700}
+                        >
+                          <Button id="learn-more-btn" variant="outline-light">
+                            Learn More
+                          </Button>
+                        </Link>
                         <Button
                           id="login-btn"
                           variant="outline-light"
@@ -71,7 +80,7 @@ function LoginPage() {
           <div className="blue-div">
             <Container id="about-container" fluid>
               <Row>
-                <Col md="auto" className="image-col">
+                <Col md="auto" className="about-col">
                   <Card className="img-card">
                     <img
                       fluid="true"
@@ -82,7 +91,7 @@ function LoginPage() {
                     ></img>
                   </Card>
                 </Col>
-                <Col className="text-center">
+                <Col className="text-center about-col">
                   <p>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     Sequi iusto, sint ullam exercitationem error tempore
@@ -110,7 +119,7 @@ function LoginPage() {
           <div className="white-div">
             <Container id="about-container" fluid>
               <Row>
-                <Col className="text-center">
+                <Col className="text-center about-col">
                   <p>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     Sequi iusto, sint ullam exercitationem error tempore
@@ -132,7 +141,7 @@ function LoginPage() {
                     maiores natus!
                   </p>
                 </Col>
-                <Col md="auto" className="image-col">
+                <Col md="auto" className="about-col">
                   <Card className="img-card">
                     <img
                       fluid="true"
@@ -142,6 +151,45 @@ function LoginPage() {
                       alt="Pup and Trainer High Five"
                     ></img>
                   </Card>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+          <div className="blue-div">
+            <Container id="about-container" fluid>
+              <Row>
+                <Col md="auto" className="about-col">
+                  <Card className="img-card">
+                    <img
+                      fluid="true"
+                      className="img-left"
+                      id="pup-high-five"
+                      src={PupHighFive}
+                      alt="Pup and Trainer High Five"
+                    ></img>
+                  </Card>
+                </Col>
+                <Col className="text-center about-col">
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Sequi iusto, sint ullam exercitationem error tempore
+                    perferendis facilis illum vel dignissimos minima commodi ut
+                    distinctio labore illo consequuntur deleniti odio qui!
+                  </p>
+                  <p>
+                    Nihil rerum mollitia atque suscipit facilis similique,
+                    ducimus officia, ratione obcaecati voluptate laborum sit
+                    maxime delectus? Dolore veritatis debitis neque reiciendis
+                    maiores adipisci cumque. Quisquam aspernatur adipisci illum
+                    officiis nam?
+                  </p>
+                  <p>
+                    Nobis delectus nesciunt, at blanditiis sit quos impedit.
+                    Dolores, nesciunt, doloremque nihil laborum soluta,
+                    dignissimos earum odio aut aliquam nam omnis nisi.
+                    Architecto est, animi necessitatibus perferendis suscipit
+                    maiores natus!
+                  </p>
                 </Col>
               </Row>
             </Container>
